c8652eee0565dc10ea675c773a5af5add0c5abc8
yonatan bendahan <yonatan.bendahan@gmail.com>
2020-07-06 08:48:28 +0300

    remove unused import

--- src/pages/Institution/MediaItem.tsx
+++ src/pages/Institution/MediaItem.tsx
@@ -6,7 +6,6 @@ import { useTheme } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
 import {
   LineChart,
   Line,
